a38612e9333cb3c322f845f66520f5b4e9d48130
James Kalisz <james.kalisz@tdsb.on.ca>
2026-04-21 10:42:58 -0400

    update files

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 1. Create a method called _stringSwap()_.  It will take a String array and two integers as parameters.  Swap the two values in the array at the indexes provided by the parameters. Return nothing.
 
 2. Create a method called _reverse()_.  It will take in a integer array.  Reverse the array.  Return nothing.
+
+3. Create a method called _toWordArray()_.  It will take in a String and return a String array with each word in its own index. Remove all empty Strings.
+
+
--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,6 +1,81 @@
-
 class Notes {
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 
+		int a;
+		int b;
+		a = 5;
+		b = 10;
+
+		// a, b = b, a; DONT DO THIS
+
+		//create a temp variable
+		int temp; 
+		temp = a;
+		a = b;
+		b = temp;
+
+		int[] arr = {1, 3, 5, 7};
+
+		//swap the values at index 1 and 2
+
+		temp = arr[1];
+		arr[1] = arr[2];
+		arr[2] = temp;
+
+
+		//split
+
+		//add and delete to array
+		
+		//adding to an array
+/*
+		int[] arr = {1, 5, 7, 9, 2};
+		//{1, 5, 7, 9, 15, 2}
+		int num = 15; //to be added to the array
+		int pos = 5; //index we want to add it at.
+
+
+
+		int[] arr2 = new int[arr.length + 1];
+		int index = 0; //this the index to keep track of the old array
+
+		for (int i = 0; i < arr2.length; i++) {
+			if (i != pos){
+				arr2[i] = arr[index];
+				index++;
+			}
+			else {
+				arr2[i] = num;
+			}
+		}
+
+		arr = arr2; //replacing the reference of the array.
+
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+*/
+
+		//Removing an array
+
+		arr = new int[]{1, 5, 8, 2, 3, 4, 7};
+		int index = 2; //index of the element we want to remove
+
+		int index2 = 0;
+		int[] arr2 = new int[arr.length - 1];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (i != index) {
+				arr2[index2] = arr[i];
+				index2++;
+			}
+		}
+
+		arr = arr2;
+
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
 	}
 }
