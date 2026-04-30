d92132847c362bee279acb9db46f04ba0932b23a
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-04-30 14:42:31 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,39 @@ public class PracticeProblem {
 
 	}
 
+	public static String[] addString(String[] arr, String str, int idx) {
+        int origIdx = 0;
+        String[] arr2 = new String[arr.length+1];
+        for (int i = 0; i < arr2.length; i++) {
+            if (i==idx) {
+                arr2[i] = str; 
+            } else {
+                arr2[i] = arr[origIdx];
+                origIdx++;
+                
+            }
+        }
+        return arr2;
+    }
+    
+    public static char[] removeChars(char[] arr, char letter) {
+        
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == letter) {
+                count++;
+            }
+        }
+        
+        char[] arr2 = new char[arr.length-count];
+        int newIdx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != letter) {
+                arr2[newIdx] = arr[i];
+                newIdx++;
+            }
+        }
+        return arr2;
+    }
+
 }
